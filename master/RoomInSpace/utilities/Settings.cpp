7477457ccc30acb101ac34a7a2fcc34cf2d14af1
Fumeng-Yang <yang.form@gmail.com>
2016-12-08 17:07:34 -0500

    merge

--- master/RoomInSpace/utilities/Settings.cpp
+++ master/RoomInSpace/utilities/Settings.cpp
@@ -12,7 +12,7 @@ Settings settings;
 void Settings::loadSettingsOrDefaults() {
    QSettings s("Room in Space", "Room in Space");
    renderMode = s.value("renderMode", COMPUTER_MODE).toInt();
-   cameraFov  = s.value("cameraFov", 180).toDouble();
+   cameraFov  = s.value("cameraFov", 150).toDouble();
    cameraNear = s.value("cameraNear", -100).toDouble();
    cameraFar  = s.value("cameraFar", 100).toDouble();
    windowMode = s.value("windowMode", OverUnder).toInt();
